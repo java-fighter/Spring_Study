ed131719195911484beca8484dc260249de5b9b9
java-fighter <hotcola1111@gmail.com>
2022-12-31 02:51:05 +0900

    commit test

--- spring-mvc/src/main/java/com/example/demo/controller/RegisterController.java
+++ spring-mvc/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.model.UserForm;
+import com.example.demo.domain.service.UsersRegisterService;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Controller
 public class RegisterController {
+    
+    @Autowired
+    private UsersRegisterService usersRegisterService;
 
     @GetMapping("/form")
     private String readForm(@ModelAttribute UserForm userForm) {
@@ -19,13 +27,22 @@ public class RegisterController {
     }
 
     @PostMapping("/form")
-    private String confirm(@Validated(UserForm.Groups.class) @ModelAttribute UserForm userForm, BindingResult result, Model model) {
+    private String confirm(@Validated(UserForm.Groups.class) @ModelAttribute UserForm user, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
             // エラーがある場合、form.htmlに戻る
             return "form";
         }
 
+     // 登録処理
+        if(usersRegisterService.isValid(user, result)) {
+            return "form";
+        } 
+            
+        usersRegisterService.register(user);
+            
         return "confirm";
+                
     }
+    
 }
---
+++ spring-mvc/src/main/java/com/example/demo/domain/service/UsersRegisterService.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.example.demo.domain.entity.Users;
+import com.example.demo.domain.model.UserForm;
+import com.example.demo.domain.repository.UsersRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class UsersRegisterService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+    
+    public boolean isValid(UserForm userForm, BindingResult result){
+        if(usersRepository.existsByEmail(userForm.getEmail())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void register(final UserForm userForm) {
+        
+        Users entity = new Users();
+        entity.setName(userForm.getName());
+        entity.setEmail(userForm.getEmail());
+        entity.setAge(userForm.getAge());
+        entity.setNote(userForm.getNote());
+        usersRepository.save(entity);
+    }
+}
